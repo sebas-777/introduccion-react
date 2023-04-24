5f7876207b311ab89526a2e553fd2528b5326864
sebas-777 <sebastiancast06@hotmail.com>
2023-04-23 21:03:11 -0500

    marcar como completado y eliminar

--- src/App.js
+++ src/App.js
@@ -34,7 +34,21 @@ function App() {
     })
 
     
-  }
+  } 
+
+  const completeTodo = (text) =>{
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos=[...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) =>{
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos=[...todos];
+    newTodos.splice(todoIndex,1);
+    setTodos(newTodos);
+  };
 
   return (
    <React.Fragment>
@@ -48,7 +62,13 @@ function App() {
       />
       <TodoList>
         {searchedTodos.map(todo =>(
-            <TodoItem key={todo.text} text={todo.text} />
+            <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+            />
         ))}
       </TodoList>
       <CreateTodoButton />
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,19 +3,11 @@ import './TodoItem.css'
 
 function TodoItem(props){
    
-    const  onComplete =() =>{
-        alert('Ya completeste el todo ' + props.text)
-    };
-   
-    const  onDelete =() =>{
-        alert('Borratse el todo ' + props.text)
-    };
-   
     return(
         <li className="TodoItem">
             <span 
             className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-            onClick={onComplete}
+            onClick={props.onComplete}
             >
             √
             </span>
@@ -24,7 +16,7 @@ function TodoItem(props){
             </p>
             <span 
                 className="Icon Icon-delete"
-                onClick={onDelete}
+                onClick={props.onDelete}
             >
                 X
             </span>
